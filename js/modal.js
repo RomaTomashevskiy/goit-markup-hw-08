8800550f3282f54f36db4f5d339da249086b9e41
RomaTomashevskiy <romatomashevskiy@ukr.net>
2021-12-25 14:30:40 +0200

    домашка1

--- js/modal.js
+++ js/modal.js
@@ -12,17 +12,5 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
-(() => {
-  const refs = {
-    openMobalBtn: document.querySelector('[data-mobal-open]'),
-    closeMobalBtn: document.querySelector('[data-mobal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
 
-  refs.openMobalBtn.addEventListener('click', toggleModal);
-  refs.closeMobalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
